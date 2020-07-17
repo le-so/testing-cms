3da60b0acae4a913e4bda3751daf9b7846bfd3a0
le-so <kasi_pavan@outlook.com>
2020-07-17 16:50:47 +0530

    [Font]: Added DM Font Family as primary.

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,8 +11,8 @@ import { useStaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 
-// import './layout.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../static/css/global.css'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
